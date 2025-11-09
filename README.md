efbb8effa346e8b4969a9c11b566f6f982bbb590
komeil <komeilmohammadian1376.web@gmail.com>
2025-11-09 20:54:33 +0330

    feat(fix package configuration)

---
+++ README.md
@@ -0,0 +1,2 @@
+# km-icon
+Description:A modern, open-source collection of web-ready icon fonts — perfect for developers, designers, and anyone building user interfaces.
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import lib from './lib';
+
+export const directlyTestMethod = () => {
+  return 'method working without problem';
+};
+
+const kmTestTemplate = lib;
+export default kmTestTemplate;
